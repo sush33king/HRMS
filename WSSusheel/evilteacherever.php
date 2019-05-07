ffd32afa26ee76781a737cbb205e4e4188020658
chung97 <chung971004@gmail.com>
2019-05-07 15:10:52 +0800

    style

--- WSSusheel/evilteacherever.php
+++ WSSusheel/evilteacherever.php
@@ -4,6 +4,6 @@ print "Hello this is from susheels apache server. <br>
        This is from print function <br>";
 
 echo "Hello this is from susheels apache server. <br>
-This is from echo function style is the best teacher give us less work pls is like hell alr ok come on";
+This is from echo function style is the worst database not yet finish teacher give us less work pls is like hell alr ok come on";
 
 ?>
