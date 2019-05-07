99a1c44f0d32c75bca39145d28bb2fed8ce583f4
sush33king <sush33king@gmail.com>
2019-05-07 15:14:19 +0800

    corrected 2

--- WSSusheel/evilteacherever.php
+++ WSSusheel/evilteacherever.php
@@ -4,11 +4,6 @@ print "Hello this is from susheels apache server. <br>
        This is from print function <br>";
 
 echo "Hello this is from susheels apache server. <br>
-<<<<<<< HEAD
 This is from echo function style is the best teacher give us less work pls is like hell alr ok come on" +
 "as;djasdlkajlksdjsad";
-=======
-
-
->>>>>>> ffd32afa26ee76781a737cbb205e4e4188020658
 ?>
