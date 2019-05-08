d660ebd0410b789b9908ba87d7ce8896e946b7a4
baijian <baijian743@gmail.com>
2019-05-08 13:18:41 +0800

    tan

--- WSbaijian/MyFirstPHPProgram.php
+++ WSbaijian/MyFirstPHPProgram.php
@@ -6,4 +6,16 @@ this is from print function <br>";
 echo"hello this is from baijian apache server. <br>
 this is from print function <br>";
 
+/**1.PHP - Variable Types **/ 
+/**integers**/
+$int_var = 12345;
+$another_int = -12345 + 12345;
+/**Doubles**/
+<?php
+   $many = 2.2888800;
+   $many_2 = 2.2111200;
+   $few = $many + $many_2;
+   
+   print("$many + $many_2 = $few <br>");
+?>
 ?>
