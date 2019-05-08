ec9919ceccf17aa78825372b71fb491310c5f4ea
tohxwaijie <tohxwaijie@gmail.com>
2019-05-08 14:53:57 +0800

    test

--- WSChua/my.first.php.project.php
+++ WSChua/my.first.php.project.php
@@ -1,7 +1,7 @@
 <?php
 
 
-/*intergers*/
+/*intergers and operators*/
 $i = 1 ;
 $i = $i + 3;
 $j =999999999999999999999999E+99;
@@ -10,6 +10,6 @@ echo $i * $j;
 /*string*/
 $s1 = "hello";
 $s2 = "friend";
-
+echo $s1 . $s2
 ?>
 
