8c68141c538e61d1e4688ddc21b7db928852ff92
tohxwaijie <tohxwaijie@gmail.com>
2019-05-08 14:14:57 +0800

    test

--- WSChua/my.first.php.project.php
+++ WSChua/my.first.php.project.php
@@ -1,27 +1,7 @@
 <?php
 
-echo '<table border="1">';
-
-echo "<tr>";
-echo "<td>";
-
-echo "hi my name is wai jie . <br>
-       this is my work <br>" ;
-
-echo "</td>";
-echo "</tr>";
-
-echo "<tr>";
-echo "<td>";
-
-echo "hi my name is wai jie.  <br>
-       this is my work <br>" ;
-
-       echo "</tr>";
-echo "</td>";
-
-echo "</table>";
-
-
+$hello = 'Hello World!';
+echo $hello;
 
 ?>
+
