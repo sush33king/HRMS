921a74b52ded9a976b1ed2be143aaaca227ce4ba
chung97 <chung971004@gmail.com>
2019-05-08 15:09:22 +0800

    php

--- WSChung/Myfirstphpprogram.php
+++ WSChung/Myfirstphpprogram.php
@@ -5,8 +5,30 @@ print "Hello this is from chung Apache server. <br>
 
 echo "Hello this is from chung Apache server.  <br>
        This is from echo function <br>" ;
+
+
+$hello = 'Hello World!';
+echo $hello . 'Susheel';
+
+/*integers*/
+$i = 1 ;
+$i = $i + 3 ;
+$j = 5;
+echo $i * $j; 
+
+/*string*/
+$s1 = 'Hello' ;
+$s2 = 'Friend' ;
+$s3 = $s1 . $s2 ;
+
+$s1 = "" ;
+$s2 = "" ;
+
+echo $s3 ;
 ?>
 
+
+ 
 
 
 
