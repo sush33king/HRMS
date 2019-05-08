c69a89066dc711b078c72044af5d1a3349af7a8b
sush33king <sush33king@gmail.com>
2019-05-08 15:10:13 +0800

    latest commit

---
+++ WSSusheel/Class1.php
@@ -0,0 +1,19 @@
+<?php
+
+/*integers and operators*/
+$i = 1;
+$i = $i + 3;
+$j = 5;
+echo $i * $j;
+
+/*String */
+$s1 = "hello ";
+$s2 = "Friend";
+$s3 = $s1 . $s2;
+
+$s1 = "";
+$s2 = "";
+
+echo $s3;
+
+?>
