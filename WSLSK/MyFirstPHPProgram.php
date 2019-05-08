680d22cb02513e8232c6b2adf126a677ea7a9371
chung97 <chung971004@gmail.com>
2019-05-08 14:10:11 +0800

    hhhhhhhhhhhhhhhhhhhh

---
+++ WSLSK/MyFirstPHPProgram.php
@@ -0,0 +1,9 @@
+<?php
+
+print "Hello this is from LSK apache server. <br>
+       This is from print function <br>";
+
+
+       echo "Hello this is from LSK Apache server.  <br>
+       This is from echo function <br>" ;
+?>
