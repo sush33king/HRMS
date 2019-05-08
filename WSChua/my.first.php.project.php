c5b49ba25167ae73357a3e074917c39992d47277
tohxwaijie <tohxwaijie@gmail.com>
2019-05-08 13:45:36 +0800

    test2

--- WSChua/my.first.php.project.php
+++ WSChua/my.first.php.project.php
@@ -1,9 +1,27 @@
 <?php
 
-print "CHUA Compeleted. <br> 
-       this is from print function <br>" ; 
+echo '<table border="1">';
 
-echo "CHUA Compeleted. <br>
-      this is echo function <br>";
+echo "<tr>";
+echo "<td>";
+
+echo "Hello this is from Randy Apache server. <br>
+       This is from print function <br>" ;
+
+echo "</td>";
+echo "</tr>";
+
+echo "<tr>";
+echo "<td>";
+
+echo "Hello this is from Randy Apache server.  <br>
+       This is from echo function asdasd<br>" ;
+
+       echo "</tr>";
+echo "</td>";
+
+echo "</table>";
+
+
 
 ?>
