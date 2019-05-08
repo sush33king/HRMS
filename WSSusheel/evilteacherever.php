394fb926075de59113bc37ace29b8180bc173808
chung97 <chung971004@gmail.com>
2019-05-08 13:50:29 +0800

    hi

--- WSSusheel/evilteacherever.php
+++ WSSusheel/evilteacherever.php
@@ -5,8 +5,9 @@ echo '<table border="1">';
 echo "<tr>";
 echo "<td>";
 
-echo "Hello this is from Randy Apache server. <br>
-       This is from print function <br>" ;
+echo "Hello this is from Randy ku ku ku ku ku Apache server. <br>
+       This is from print function <br>
+       I am the best student <br>" ;
 
 echo "</td>";
 echo "</tr>";
@@ -14,8 +15,9 @@ echo "</tr>";
 echo "<tr>";
 echo "<td>";
 
-echo "Hello this is from Randy Apache server.  <br>
-       This is from echo function asdasd<br>" ;
+echo "Hello this is from Randy ku ku ku ku ku Apache server.  <br>
+       This is from echo function asdasd <br>
+       I am the best student <br>" ;
 
        echo "</tr>";
 echo "</td>";
