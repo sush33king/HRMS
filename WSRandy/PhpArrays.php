b596c2c55bdf1a4f2e723295cb74d873ac150986
R_Kjz <randykjz@gmail.com>
2019-05-09 14:52:11 +0800

    l

--- WSRandy/PhpArrays.php
+++ WSRandy/PhpArrays.php
@@ -1,4 +1,17 @@
 <?php
+
 $Brands = array("Asus", "Evga", "Zotac");
-echo "I would prefer " . $Brands[0] . ", " . $Brands[1] . " and " . $Brands[2] . ".";
+echo " <b> I would prefer </b>" . $Brands[0] . ", " . $Brands[1] . " and " . $Brands[2] . ".";
+
+$Brands = array("Asus", "Evga", "Zotac");
+echo count($Brands);
+
+$Brands = array("Asus", "Evga", "Zotac");
+$arrlength = count($Brands);
+
+for($x = 0; $x < $arrlength; $x++) {
+    echo $Brands[$x];
+    echo "<br>";
+}
+
 ?>
