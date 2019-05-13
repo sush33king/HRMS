b66347a2d8d389805289e8f2aa610e2186f21dcb
sush33king <zoeycapricorn@outlook.com>
2019-05-13 13:46:34 +0800

    zoey php assignment

---
+++ WSZoey/Assignment.php
@@ -0,0 +1,12 @@
+<?php
+$processor = array (
+    array("AMD Ryzen 5 2600",650,20),
+    array("Intel Core i5-8400",800,20),
+    array("Intel Core i7-8700K",1700,20),
+    array("AMD Ryzen 7 2700X",1300,20)
+    );
+
+echo $processor[0][0]
+echo $processor[1][0]
+echo $processor[2][0]
+echo $processor[3][0]
