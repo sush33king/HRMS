26c242efe8fd1fe79adc43eba8ad5ab0fdcc9144
nerf3388 <nerf3388@outlook.com>
2019-05-13 15:34:04 +0800

    NOOBS

--- WSZoey/Assignment.php
+++ WSZoey/Assignment.php
@@ -6,13 +6,12 @@ $products = array (
     array("AMD Ryzen 7 2700X",1300,10)
     );
 
-echo "Product:" . $products[0][0] . ", Price:" . $products[0][1] . ", Quantity:" . $products [0][2] . "<br> <br>";
+echo "Product:" . $products[0][0] . ", Price:" . $products[0][1] . ", Quantity:" . $products[0][2] . "<br> <br>";
 echo "Product:" . $products[1][0] . ", Price:" . $products[1][1] . ", Quantity:" . $products[1][2]  . "<br> <br>";
 echo "Product:" . $products[2][0] . ", Price:" . $products[2][1] . ", Quantity:" . $products[2][2]  . "<br> <br>";
 echo "Product:" . $products[3][0] . ", Price:" . $products[3][1] . ", Quantity:" . $products[3][2]  . "<br> <br>";
 
-echo var_dump ($products[0]);
-
+echo var_dump ($products);
 
 $total=0;
 
@@ -24,5 +23,18 @@ $shoppingcart[0] = $total;
 $shoppingcart[1] = $products;
 
 echo "<br>";
+
 echo $total;
+
+echo "<br>";
+
+echo "ShoppingCart";
+
+echo "<br>";
+
+echo var_dump($shoppingcart);
+
+echo '<textarea rows="4" cols="50">
+At w3schools.com you will learn how to make a website. We offer free tutorials in all web development technologies. 
+</textarea>';
 ?>
