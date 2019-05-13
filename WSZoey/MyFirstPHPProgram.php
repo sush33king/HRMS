ed7d2c886ace2f35fb5af157cf9328d0f5c9e09c
sush33king <zoeycapricorn@outlook.com>
2019-05-13 11:05:17 +0800

    Zoey's PHP

--- WSZoey/MyFirstPHPProgram.php
+++ WSZoey/MyFirstPHPProgram.php
@@ -1,4 +1,42 @@
 <?php
 // Display greeting message
-echo "Hello, world!";
+echo "Good Morning!";
+?>
+
+<?php
+// Comments
+// This is a single line comment
+# This is also a single line comment
+/*This is a multi
+line comment */
+echo "Good Afternoon!";
+?>
+
+<?php
+// Declaring variables
+//$var_name = value
+$txt = "Good Evening!";
+$number = 14;
+ 
+// Displaying variables value
+echo $txt;  // Output: Good Evening!
+echo $number; // Output: 14
+?>
+
+<?php
+// Defining constant
+// define() function accepts two arguments: the name of the constant, and its value
+define("SITE_URL", "www.facebook.com");
+ 
+// Using constant
+echo 'Thank you for visiting - ' . SITE_URL;
+?>
+
+<?php
+// Displaying string of text
+echo "Hello People!";
+
+// Displaying HTML code
+echo "<h4>This text is black.</h4>";
+echo "<h4 style='color: red;'>This text is red.</h4>";
 ?>
