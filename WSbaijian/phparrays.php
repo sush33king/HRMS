2ccad19425214a5727805b2f0045d36229d9a054
baijian <baijian743@gmail.com>
2019-05-13 10:48:37 +0800

    tan

--- WSbaijian/phparrays.php
+++ WSbaijian/phparrays.php
@@ -1,7 +1,30 @@
 <?php
-$game = array("cso", "gta_5", "lol");
-echo "I like " . $game[cso] . ", " . $game[gta_5] . " and " . $game[lol] . ".";
+//$game = array("cso", "gta_5", "lol");
+//echo "I like " . $game[cso] . ", " . $game[gta_5] . " and " . $game[lol] . ".";
 
-$game = array("cso", "gta_5", "lol");
-echo count($game);
+//$game = array("cso", "gta_5", "lol");
+//echo count($game);
+
+//$game = array("cso", "gta_5", "lol");
+
+//$arrlength = count($game);
+
+/*for($x = 0; $x < $arrlength; $x++) {
+    echo '$x = ' . $x . "<br>";
+    echo $game[$x];
+    echo "<br>";
+}*/
+
+/*$age = array("tan"=>"20", "fong"=>"19", "Joe"=>"23");
+echo "tan is " . $age['tan'] . " years old.";
+
+$age = array("tan"=>"20", "fong"=>"19", "Joe"=>"23");
+
+foreach($age as $x => $x_value) {
+    echo "Key=" . $x . ", Value=" . $x_value;
+    echo "<br>";
+}*/
+
+array
+      $shoppiycart = array
 ?>
