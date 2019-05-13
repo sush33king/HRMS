10394f07e3c3f5a049d33d3f5af592132f035252
sush33king <zoeycapricorn@outlook.com>
2019-05-13 15:08:34 +0800

    zoey assignment

--- WSZoey/Assignment.php
+++ WSZoey/Assignment.php
@@ -1,21 +1,28 @@
 <?php
-$processor = array (
+$products = array (
     array("AMD Ryzen 5 2600",650,10),
     array("Intel Core i5-8400",800,10),
     array("Intel Core i7-8700K",1700,10),
     array("AMD Ryzen 7 2700X",1300,10)
     );
 
-echo "Product:" .$processor[0][0]." Price:" .$processor[0][1]." Quantity:" .$processor.[0][2]."<br>";
-echo "Product:" .$processor[1][0]." Price:" .$processor[1][1]." Quantity:" .$processor.[1][2]."<br>";
-echo "Product:" .$processor[2][0]." Price:" .$processor[2][1]." Quantity:" .$processor.[2][2]."<br>";
-echo "Product:" .$processor[3][0]." Price:" .$processor[3][1]." Quantity:" .$processor.[3][2]."<br>";
+echo "Product:" . $products[0][0] . ", Price:" . $products[0][1] . ", Quantity:" . $products [0][2] . "<br> <br>";
+echo "Product:" . $products[1][0] . ", Price:" . $products[1][1] . ", Quantity:" . $products[1][2]  . "<br> <br>";
+echo "Product:" . $products[2][0] . ", Price:" . $products[2][1] . ", Quantity:" . $products[2][2]  . "<br> <br>";
+echo "Product:" . $products[3][0] . ", Price:" . $products[3][1] . ", Quantity:" . $products[3][2]  . "<br> <br>";
 
-echo var_dump ($processor[0]);
+echo var_dump ($products[0]);
+
+
 $total=0;
-    for($x = 0; $x < count($processor); $x++) {
-        $total = $total + $processor[$x][1];
-    }
-    echo "<br>";
-    echo $total;
+
+for($x = 0; $x < count($products); $x=$x+1) {
+    $total = $total + ($products[$x][1] * $products[$x][2]);
+}
+
+$shoppingcart[0] = $total;
+$shoppingcart[1] = $products;
+
+echo "<br>";
+echo $total;
 ?>
