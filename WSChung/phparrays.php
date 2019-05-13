79c7230eec95b73d9c23a8a7885a159f96c3ecd3
chung97 <chung971004@gmail.com>
2019-05-13 15:06:16 +0800

    phphphphphph

--- WSChung/phparrays.php
+++ WSChung/phparrays.php
@@ -41,5 +41,14 @@ echo $phone[0][0].": price: ".$phone[0][1].", quantity: ".$phone[0][2].".<br>";
 echo $phone[1][0].": price: ".$phone[1][1].", quantity: ".$phone[1][2].".<br>";
 echo $phone[2][0].": price: ".$phone[2][1].", quantity: ".$phone[2][2].".<br>";
 
+echo var_dump ($phone)[0] ;
+$total = 0 ;
+   for ($x = 0; $x < count($phone); $x++ ) {
+     $total = $total + ($phone[$x][1] * $phone[$x][2]);
+   }  
+
+echo "<br>" ;
+echo "total cost = " . "$total" ;
+
 
 ?>
