7fb71dc29ff4765bce77dec65af945025f67c92b
R_Kjz <randykjz@gmail.com>
2019-05-13 15:24:52 +0800

    k

--- WSRandy/PhpArrays.php
+++ WSRandy/PhpArrays.php
@@ -31,7 +31,7 @@ for($x = 0; $x < $TotalNoofShoppingCart; $x++) {
     }
 }
 echo "yaaay out of loop";*/
-
+//1. creating products array
 $Products = array(
     array("Asus",1800,5),
     array("Evga",1900,4),
@@ -39,7 +39,9 @@ $Products = array(
     array("Nvidia",1800,7),
     array("Galax",1500,6)
     );
-$Total = 0;
+
+//2. loop through products array and calculate total overall cost for products
+    $Total = 0;
 
 for($z = 0; $z < count($Products) ; $z++) {
     $Total = $Total + $Products[$z][1] * $Products[$z][2];
@@ -47,20 +49,18 @@ for($z = 0; $z < count($Products) ; $z++) {
 echo "<br>";
 
 $TotalCost = $Total;
+
+//3. Create a shoppingcart variable to hold/contain 1 and 2
 $ShoppingCart[0] = $TotalCost;
 $ShoppingCart[1] = $Products;
 
 echo var_dump($ShoppingCart);
 
-echo $ShoppingCart[0][0].": Price: ".$ShoppingCart[0][1].", Quantity: ".$ShoppingCart[0][2].".<br>";
-echo $ShoppingCart[1][0].": Price: ".$ShoppingCart[1][1].", Quantity: ".$ShoppingCart[1][2].".<br>";
-
-/*echo $ShoppingCart[2][0].": Price: ".$Price[2][1].", Quantity: ".$Quantity[2][2].".<br>";
-echo $ShoppingCart[3][0].": Price: ".$Price[3][1].", Quantity: ".$Quantity[3][2].".<br>";
-echo $ShoppingCart[4][0].": Price: ".$Price[4][1].", Quantity: ".$Quantity[4][2].".<br>";*/
-
-//echo var_dump ($ShoppingCart(0));
-//$Total
+echo $Products[0][0].": Price: ".$Products[0][1].", Quantity: ".$Products[0][2].".<br>";
+echo $Products[1][0].": Price: ".$Products[1][1].", Quantity: ".$Products[1][2].".<br>";
+echo $Products[2][0].": Price: ".$Products[2][1].", Quantity: ".$Products[2][2].".<br>";
+echo $Products[3][0].": Price: ".$Products[3][1].", Quantity: ".$Products[3][2].".<br>";
+echo $Products[4][0].": Price: ".$Products[4][1].", Quantity: ".$Products[4][2].".<br>";
 
 echo ' <textarea rows="4" cols="50">
 At w3schools.com you will learn how to make a website. We offer free tutorials in all web development technologies.
