77af20637459c6e6bcc16aa80785caf651f6cad4
sush33king <zoeycapricorn@outlook.com>
2019-05-14 16:12:58 +0800

    zoey loop

--- WSZoey/Assignment2.php
+++ WSZoey/Assignment2.php
@@ -6,10 +6,10 @@ $products = array (
     array("AMD Ryzen 7 2700X",1300,10)
     );
 
-echo "Product:" . $products[0][0] . ", Price:" . $products[0][1] . ", Quantity:" . $products [0][2] . "<br> <br>";
-echo "Product:" . $products[1][0] . ", Price:" . $products[1][1] . ", Quantity:" . $products[1][2]  . "<br> <br>";
-echo "Product:" . $products[2][0] . ", Price:" . $products[2][1] . ", Quantity:" . $products[2][2]  . "<br> <br>";
-echo "Product:" . $products[3][0] . ", Price:" . $products[3][1] . ", Quantity:" . $products[3][2]  . "<br> <br>";
+//echo "Product:" . $products[0][0] . ", Price:" . $products[0][1] . ", Quantity:" . $products [0][2] . "<br> <br>";
+//echo "Product:" . $products[1][0] . ", Price:" . $products[1][1] . ", Quantity:" . $products[1][2]  . "<br> <br>";
+//echo "Product:" . $products[2][0] . ", Price:" . $products[2][1] . ", Quantity:" . $products[2][2]  . "<br> <br>";
+//echo "Product:" . $products[3][0] . ", Price:" . $products[3][1] . ", Quantity:" . $products[3][2]  . "<br> <br>";
 
 $total=0;
 $z = count($products);
@@ -20,16 +20,21 @@ for($x = 0; $x <$z ; $x=$x+1) {
 $shoppingcart[0] = $total;
 $shoppingcart[1] = $products;
 
-$shoppingcartcontent = 0;
+$productname = 0;
+$productprice = 0;
+$productquantity = 0;
 $int = count($shoppingcart[1]);
 $whileint = 0;
 
 while($whileint < $int) {
-    $shoppingcartcontent = .$shoppingcart[1][$whileint][0]. .$shoppingcart[1][$whileint][1]. .$shoppingcart[1][$whileint][2]. ;
+    $productname = $shoppingcart[1][$whileint][0];
+    $productprice = $shoppingcart[1][$whileint][1];
+    $productquantity = $shoppingcart[1][$whileint][2];
+    echo $productname;
+    echo " " .$productprice;
+    echo " " .$productquantity. "<br> <br>";
     $whileint++;
 }
-
-echo $shoppingcartcontent;
 
 
 //$tempshoppingcart = array ($shoppingcart[0], $shoppingcart[1]);
