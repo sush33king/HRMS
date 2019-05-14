5697dfeb4590b49aefb54224b3334007bfa7c527
R_Kjz <randykjz@gmail.com>
2019-05-14 14:56:52 +0800

    s

---
+++ WSRandy/TempShoppingCart.php
@@ -0,0 +1,39 @@
+<?php
+
+$Products = array(
+    array("Asus",1800,5),
+    array("Evga",1900,4),
+    array("Zotac",1500,7),
+    array("Nvidia",1800,7),
+    array("Galax",1500,6)
+    );
+
+//2. loop through products array and calculate total overall cost for products
+    $Total = 0;
+
+    $p = count($Products);
+
+for($z = 0; $z < $p ; $z++) {
+    $Total = $Total + $Products[$z][1] * $Products[$z][2];
+}
+echo "<br>";
+
+$TotalCost = $Total;
+
+//3. Create a shoppingcart variable to hold/contain 1 and 2
+$ShoppingCart[0] = $TotalCost;
+$ShoppingCart[1] = $Products;
+
+echo var_dump($ShoppingCart);
+
+echo $Products[0][0].": Price: ".$Products[0][1].", Quantity: ".$Products[0][2].".<br>";
+echo $Products[1][0].": Price: ".$Products[1][1].", Quantity: ".$Products[1][2].".<br>";
+echo $Products[2][0].": Price: ".$Products[2][1].", Quantity: ".$Products[2][2].".<br>";
+echo $Products[3][0].": Price: ".$Products[3][1].", Quantity: ".$Products[3][2].".<br>";
+echo $Products[4][0].": Price: ".$Products[4][1].", Quantity: ".$Products[4][2].".<br>";
+
+echo ' <textarea rows="4" cols="50">
+At w3schools.com you will learn how to make a website. We offer free tutorials in all web development technologies.
+</textarea>';
+
+?>
