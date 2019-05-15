86d1e4c72e53dbbd6f2e47deebc3db5273cd2827
sush33king <zoeycapricorn@outlook.com>
2019-05-15 15:42:29 +0800

    zoey php

--- WSZoey/Assignment4.php
+++ WSZoey/Assignment4.php
@@ -1,13 +1,20 @@
 <?php
-$Hash = array("#");
+$Hash = "#";
 $HashLimit = 100;
 $WhileInt = 0;
+$HashCount = 0;
+$HashControl = $HashCount + 1;
 
 
 while($WhileInt < $HashLimit){
     echo $Hash;
-    $WhileInt++;
+    $HashCount++;
+    if ($HashCount = $H ) {
+        echo "<br>";
+       
+    }
     
+
 }
 
 
