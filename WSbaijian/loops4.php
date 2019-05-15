79e17538451eab8173d44efd41cb687ec727fe58
baijian <baijian743@gmail.com>
2019-05-15 13:58:26 +0800

    tan

--- WSbaijian/loops4.php
+++ WSbaijian/loops4.php
@@ -2,8 +2,7 @@
    <body>
    
       <?php
-         $array = array( 1, 2, 3, 4, 5);
-         
+         $array = array(1,2,3,4,5);
          foreach( $array as $value ) {
             if( $value == 3 )continue;
             echo "Value is $value <br />";
