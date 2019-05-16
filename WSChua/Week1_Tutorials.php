b2b01a669e65e6dd51534ac6abfcb6d1a7af8be4
tohxwaijie <tohxwaijie@gmail.com>
2019-05-16 14:53:53 +0800

    test

--- WSChua/Week1_Tutorials.php
+++ WSChua/Week1_Tutorials.php
@@ -16,7 +16,21 @@ phpinfo();
 ?>
 
 
+<? php
+ 
+ for ($size = 1; $size <= 100; $size++){
 
+    for ($odd = 1; $j <= 5 -$size; $odd++){
+        echo ” “;
+        }
+        
+        for ($odd = 1; $j <= 2*$size – 1; $odd++){
+        echo(“*”);
+        }
+    echo "<br>"; 
+        }
+
+?>
 
 
 
