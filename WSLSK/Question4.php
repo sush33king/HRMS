f4d52e919079557bd38751a4e4875cfaefc11adc
nerf3388 <nerf3388@outlook.com>
2019-05-16 14:29:31 +0800

    f

--- WSLSK/Question4.php
+++ WSLSK/Question4.php
@@ -2,15 +2,10 @@
 
 for ($size = 1; $size <= 20; $size++){
 
-for ($j = 1; $j <= 5 -$size; $j++){
-echo "";
-}
-
-for ($j = 1; $j <= 2*$size - 1; $j++){
-echo("*");
-}
-
-echo "<br>";
+    for ($j = 1; $j <= 5 -$size; $j++)
+    {
+        echo "";
+    }
 }
 
 ?>
