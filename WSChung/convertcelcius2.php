436f42d0f7412cb0df6bdd6a4d91c1ee27344cb8
chung97 <chung971004@gmail.com>
2019-05-16 15:46:25 +0800

    php

---
+++ WSChung/convertcelcius2.php
@@ -0,0 +1,27 @@
+<?php
+
+
+//$_GET is an array that the server will keep in memory to store browser/user input.
+//If browser supplies input through the $_GET variablle, then this is technically termed as a GET REQUEST.
+//Check if any input variable provided through the GET variable 'x'. 
+
+if(isset($_GET['x']) )
+{
+    //1. You can directly access the contents of the input variables
+    //echo "Total: " . ($_GET['x'] + $_GET['y']);
+    //echo "<br>";
+
+    //2. Display in browser the number of inputs provided
+    echo "There are " . count($_GET) . " input variables initialized";
+    echo "<br>";
+
+    //3. Loop through the 
+    echo "The Fahrenheit is : ";
+    echo "<br>";
+ 
+    foreach ($_GET as &$value) {
+    echo ($value*9/8)+32 . "°F";
+    echo "<br>" ;
+    }
+}
+?>
