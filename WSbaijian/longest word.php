d10a867d6b2e68a4d6916e4467ab43c1879aa64f
baijian <baijian743@gmail.com>
2019-05-27 15:07:04 +0800

    tan

--- WSbaijian/longest word.php
+++ WSbaijian/longest word.php
@@ -1,7 +1,18 @@
 <?php
-$game = array("dota_2","gta_5","lol")
- foreach()
+$game = array("dota_2","gta_5","lol","superman","jason");
 
+$max = 0;
+$longestword = ""; 
+
+ for($i = 0; $i < count($game) ; $i++)
+    {
+     if(count ($game[$i]) > $max)
+        {
+         $max = count ($game[$i]);
+         $longestword = $game[$i];
+
+        }
+    }
 
 
 
--- WSbaijian/phparrays.php
+++ WSbaijian/phparrays.php
@@ -80,34 +80,4 @@ echo  "quantity" ."<b>" . $game[$x][2] . "</b>" ;
 $x++ ; 
 }*/
 
-/*MinMax.php*/
-/*$x = 15;
-$y = 8;
-$z = 25;
-
-//Min Value
-if ($x < $y And $x < $z) {
-    echo "Variable X contains the smallest value";
-}
-elseif ($y < $x And $y < $z) {
-    echo "Variable Y contains the smallest value";
-}
-
-else {
-    echo "Variable Z contains the smallest value";
-}
-
-echo "<br> <br>";
-
-//Max Value
-if ($x > $y And $x > $z){
-    echo "Variable X contains the largest value";
-}
-elseif ($y > $x And $y > $z){
-    echo "Variable Y contains the largest value";
-}
-else {
-    echo "Variable Z contains the largest value";
-}*/
-
 ?>
