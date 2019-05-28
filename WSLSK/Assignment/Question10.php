f7b003a83bb93ada4c730f002ce5a37bffdddd8e
nerf3388 <nerf3388@outlook.com>
2019-05-28 15:02:34 +0800

    f

--- WSLSK/Assignment/Question10.php
+++ WSLSK/Assignment/Question10.php
@@ -1,9 +1,22 @@
 <?php
-if(isset($_GET['x']) && isset($_GET['y']))
+if (isset($_GET['x']) AND isset($_GET['y']))
 {
-$
+    $width = $_GET['x'];
+    $height = $_GET['y'];
+
+    for ($y = 0; $y < $height;$y++)
+    {
+        for ($x = 0; $x < $width;$x++)
+        { 
+            echo "*";
+        }
+        echo "<br>";
+    }
 }
 else
-    echo 'Input variable not provided';
+{
+    echo "Please input values";
+}
+
 
 ?>
