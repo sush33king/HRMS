6808d7cc68f7787c1ba7204f43cfffe3ebe6e2d3
baijian <baijian743@gmail.com>
2019-06-10 13:10:04 +0800

    tan

--- WSbaijian/13.php
+++ WSbaijian/13.php
@@ -1,7 +1,26 @@
 <?php
-if $rom = array("1","2","3","4","5","6","7","8","9")
+    //1. Check if user supplied parameter through GET REQUEST
+    if(isset($_GET['x']))
+    {
+        //2. Assign user supplied parameter to a local variable
+        $x = $_GET['x'];
 
-foreach ($rom = array)
+        //3. split contents of $x into a string array
+        $temp_char_array = str_split($x);
+
+        //echo var_dump($temp_char_array);
+        $i = 0
+
+        $
+       
+    }
+    else
+    {
+        echo "Parameter not supplied";
+    }
+
+
+
 
 
 
