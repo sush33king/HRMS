4b79b9d83bb70c3e244accbd6811fef6f5fa5886
baijian <baijian743@gmail.com>
2019-06-10 13:17:11 +0800

    tan

--- WSbaijian/13.php
+++ WSbaijian/13.php
@@ -9,14 +9,56 @@
         $temp_char_array = str_split($x);
 
         //echo var_dump($temp_char_array);
-        $i = 0
+        $i = 0 ;
 
-        $
-       
-    }
-    else
-    {
-        echo "Parameter not supplied";
+        for ($i = 0; $i < count($temp_char_array); $i++ ) 
+        {
+
+            if ($temp_char_array[$i] == "1") 
+            {
+                echo "A" ;
+            } 
+            elseif ($temp_char_array[$i] == "2") 
+            {
+                echo "B" ;
+            } 
+            elseif ($temp_char_array[$i] == "3") 
+            {
+                echo "C" ;
+            }
+            elseif ($temp_char_array[$i] == "4") 
+            {
+                echo "D" ;
+            }
+            elseif ($temp_char_array[$i] == "5") 
+            {
+                echo "E" ;
+            }
+            elseif ($temp_char_array[$i] == "6") 
+            {
+                echo "F" ;
+            }
+            elseif ($temp_char_array[$i] == "7") 
+            {
+                echo "G" ;
+            }
+            elseif ($temp_char_array[$i] == "8") 
+            {
+                echo "H" ;
+            }
+            elseif ($temp_char_array[$i] == "9") 
+            {
+                echo "I" ;
+            }
+            else{
+                echo "Problem with parameters";
+            } 
+            }
+        
+        }
+            else
+            {
+            echo "Parameter not supplied";    
     }
 
 
