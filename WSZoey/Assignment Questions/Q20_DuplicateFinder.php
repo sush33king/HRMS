256f07a90eb226a806fc0579fc589191bd7b2b05
sush33king <zoeycapricorn@outlook.com>
2019-06-12 13:33:24 +0800

    idk how to do this shit

---
+++ WSZoey/Assignment Questions/Q20_DuplicateFinder.php
@@ -0,0 +1,15 @@
+<?php
+
+//Creating the Array
+$IntArray = array(1 , 15 , 7 , 1 , 2 , 5 , 7 , 8 , 9 , 66 , 33 , 8);
+
+//Determining the Unique Integers
+$Unique = array_unique($IntArray);
+
+//Duplicate Count
+echo "Duplicate Items Found:". count($IntCount > 1). "<br><br>";
+print_r($IntCount > 1);
+
+
+
+?>
