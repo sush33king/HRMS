0f2072df08e7b16803205bd413b326c7c31eb780
R_Kjz <randykjz@gmail.com>
2019-06-13 13:08:41 +0800

    b

---
+++ WSRandy/AssignmentQuestions/Question13.php
@@ -0,0 +1,17 @@
+<?php
+
+$letter = array('A','B','C','D','E','F','G','H','I','J','K', 'L','M','N','O','P','Q','R','S','T','U','V','W','X ','Y','Z');
+
+if(isset($_GET['No'])){
+    $number = $_GET['No'];
+
+    foreach( $letter as $value ); 
+
+echo $value;
+}
+
+
+
+
+
+?>
