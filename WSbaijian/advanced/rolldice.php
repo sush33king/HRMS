365286207435bc5d7ed12823e44d99d9667c10ab
baijian <baijian743@gmail.com>
2019-06-20 10:15:38 +0800

    tan

--- WSbaijian/advanced/rolldice.php
+++ WSbaijian/advanced/rolldice.php
@@ -1,7 +1,49 @@
 <?php
-Class rolldice
+class DiceRoll
 {
-    
 
+public function Draw1() 
+echo " __________
+      |          |
+      |    *     |
+      |          |
+      |__________| " ;
+
+public function Draw2() 
+echo " __________
+      |          |
+      |  *    *  |
+      |          |
+      |__________| " ;
+}
+
+public function Draw3() 
+echo "   __________
+        |          |
+        |  *    *  |
+        |    *     |
+        |__________|" ;
+
+public function Draw4() 
+echo " ___________
+      |           |
+      |  *    *   |
+      |  *    *   |
+      |___________| " ;
+
+public function Draw5() 
+echo " ___________
+      |   *   *   |
+      |     *     |
+      |   *   *   |
+      |___________|  " ;
+
+      public function Draw6() 
+echo " ___________
+      |   *   *   |
+      |   *   *   |
+      |   *   *   |
+      |___________|  " ;
+          
 }
 ?>
