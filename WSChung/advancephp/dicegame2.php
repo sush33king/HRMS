8da8f8deea1b8ade7b65434f111a0d81619701a9
sush33king <sush33king@gmail.com>
2019-06-25 13:21:33 +0800

    asdasd

--- WSChung/advancephp/dicegame2.php
+++ WSChung/advancephp/dicegame2.php
@@ -5,9 +5,9 @@ Class RollDice
     
     function RockAndRoll()
     {       
-        //echo '../images/dice/dice' . rand(1,6) . '.PNG';
+        //echo '/images/dice/dice' . rand(1,6) . '.PNG';
         header('Content-Type: image/png');
-        echo file_get_contents('../images/dice/dice' . rand(1,6) . '.PNG');            
+        echo file_get_contents('images/dice/dice' . rand(1,6) . '.png');            
     }
 
 }
--- WSSusheel/AdvancedProgramming/Test/TestClasses.php
+++ WSSusheel/AdvancedProgramming/Test/TestClasses.php
@@ -2,10 +2,6 @@
 
 include_once("../MyClassLibraries/RollDice.php");
 $dice = new RollDice;
-<<<<<<< HEAD
-$dice->rolldice();
-=======
 $dice->RockAndRoll();
 
->>>>>>> ad2e543f2e2983d5d69f1e70fd810df7093f3ed0
 ?>
