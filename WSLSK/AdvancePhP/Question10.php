41f144e94b6ab5269c745559488fd45978a1ad0a
nerf3388 <nerf3388@outlook.com>
2019-07-01 12:09:29 +0800

    f

---
+++ WSLSK/AdvancePhP/Question10.php
@@ -0,0 +1,33 @@
+<?php 
+  
+interface C { 
+   public function insideC(); 
+} 
+  
+interface B { 
+   public function insideB(); 
+} 
+  
+class Multiple implements B, C { 
+  
+    // Function of the interface B 
+    function insideB() { 
+        echo "I am in interface B"; 
+    } 
+  
+    // Function of the interface C 
+    function insideC() { 
+        echo "I am in interface C"; 
+    } 
+  
+    public function insidemultiple() 
+    { 
+        echo "I am in inherited class"; 
+    } 
+} 
+  
+$geeks = new Multiple(); 
+$geeks->insideC(); 
+$geeks->insideB(); 
+$geeks->insidemultiple(); 
+?>
