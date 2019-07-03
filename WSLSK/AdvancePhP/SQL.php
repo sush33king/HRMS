7843836ebaeaa2ea1a85a0aadb258d67e263edd3
nerf3388 <nerf3388@outlook.com>
2019-07-03 11:39:07 +0800

    d

--- WSLSK/AdvancePhP/SQL.php
+++ WSLSK/AdvancePhP/SQL.php
@@ -34,9 +34,47 @@ Class Diagram
         return $rs;
     }
 
-    public function addDB()
+    public function addDB($data, $tblName)
     {
+        //declare $conn as global so that addDB() can access it
+        global $conn;
 
+        //initialize variables to be used to store field names and field data
+        $fldNames = "S";
+        $fldData = "";
+
+        //loop through array and extract field names and field data from array
+        foreach($data as $key => $val)
+        {
+            
+            if($fldNames == "")
+            {
+                $fldNames = $key; 
+                $fldData = "'" . $val . "'";  
+            }   
+            else
+            {                
+                $fldNames = $fldNames .  ',' . $key;   
+                $fldData = $fldData . ',' . "'" . $val . "'";     
+            }                                 
+        }
+
+        //combine field names and field data values into sql query
+        $sql = "INSERT INTO " . $tblName . "(" . $fldNames . ") " . "VALUES(" . $fldData . ")";
+
+        //execute prepared sql query
+        $stmt = sqlsrv_query( $conn, $sql);
+
+        //if error then display error
+        if( $stmt === false )  
+        {  
+            echo "Error in query preparation/execution.\n";  
+            die( print_r( sqlsrv_errors(), true));  //this line of code terminates or ends the program completely
+        }  
+        else //if successful the display msg below
+        {
+            echo "Record successfully added!";
+        }
     }
 }
 
