2e4911c94b69d764ac1cb408e985b24d4d704188
sush33king <zoeycapricorn@outlook.com>
2019-07-03 10:27:38 +0800

    zoey

--- WSZoey/Advanced Assignment Questions/Q15_Database.php
+++ WSZoey/Advanced Assignment Questions/Q15_Database.php
@@ -2,40 +2,79 @@
 
 Class MyDatabase
 {
-    public $var1,$var2;
+    //Temp Variables
+    public $var1,$var2,$conn,$tblname;
 
+    //Construct Method
     public function __construct($var1,$var2)
     {
         $this->var1 = $var1;
         $this->var2 = $var2;
-
     }
 
+    //Connection Method
     public function makeConnection()
     {
-        sqlsrv_connect($this->var1, $this->var2);
+        //Constructing Resource Variable
+        $this->conn = sqlsrv_connect($this->var1, $this->var2);
     }
 
-    public function queryDB()
+    //Query Method
+    public function queryDB($str)
     {
-        sqlsrv_query($this->var1,$this->var2);
+        //Constructing Resource Variable
+        $stmt = sqlsrv_query($this->conn, $str);
+
+        //While loop to echo out content
+        $i = 0;
+        while($obj = sqlsrv_fetch_object($stmt))  
+       {  
+           $rs[$i++] = $obj;  
+       }
+       return $rs;
     }
 
+    //Update Method
+
+    //Add Method
+    public function addDB($tblname)
+    {
+        $sql = "SELECT * FROM $tblname";
+        $stmt = sqlsrv_query($this->conn, $sql);
+        
+        $i = 0;
+        while ($obj = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_NUMERIC))
+        {
+            $table[$i++] = $obj; 
+        }
+        return $table;
+
+        $addquery = "INSERT INTO $table ($table[0][0],$table[1][0],
+
+
+
+
+    }
+    //Delete Method
+
+    //Connection End Method
     public function closeConnection()
     {
         sqlsrv_close($this->var1, $this->var2);
     }
 }
 
-$connection = new MyDatabase("ZOELLON\SQLEXPRESS", array("Database"=>"db_hrms"));
+//Variables
+$serverName = "ZOELLON\SQLEXPRESS";
+$connectionInfo = array("Database"=>"db_hrms");
+$queryString = "SELECT * FROM tbl_employees";
+
+//Instantiation
+$connection = new MyDatabase($serverName, $connectionInfo);
 $connection1 = $connection->makeConnection();
-
-$query = new MyDatabase($connection1, "SELECT * FROM tbl_employees");
-$query1 = $query->queryDB();
-echo $query1;
-
-//$query = new MyDatabase("SELECT * FROM tbl_employees");
-//$query->queryDB();
+$rs = $connection->queryDB($queryString);
+$table = $connection->addDB("tbl_employees");
+echo var_dump($table);
 
 
 
