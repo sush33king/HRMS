1b346264b7566e4683698557a536d2ec8dfbbe81
baijian <baijian743@gmail.com>
2019-07-04 18:19:21 +0800

    tan

--- WSbaijian/advanced/ClassFiles/10.php
+++ WSbaijian/advanced/ClassFiles/10.php
@@ -1,73 +1,23 @@
 <?php
  
- interface add 
-{
-    function add ();
+ interface games{
+     public function gta_5(){
+     }
+ }
+ interface watch{
+     public function video(){
+     }
+ }
+ class x implements games,watch{
+     public function gta_5(){
+         echo "i play the gta_5";
+     }
+     public function video(){
+         echo "i watch video";
+     }
 }
 
-interface subtract
-{
-    function subtract();
-}
-
-interface multiply 
-{
-  function mutiply();
-}
-
-interface divide  
-{
-    function divide();
-}
-
-class calculator implements add , subtract
-{
-    private $one, $two;    
-
-    function __construct($num1, $num2)
-    {
-        $this->one = $num1;
-        $this->two = $num2;
-
-    } 
-
-    function add()
-    {   
-        echo  $this->one + $this->two ;          
-    }
-
-    function subtract()
-    {   
-        echo  $this->one - $this->two ;          
-    }
-
-    function mutiply()
-    {
-        echo  $this->one * $this->two ;
-    }
-    
-    function divide()
-    {
-        echo  $this->one / $this->two ;
-    }
-}
-
-
-//test section
-
-$one = new calculator(100,7);
-$one->add();
-
-function add()
-    {   
-        echo  $this->one + $this->two ;  
-        echo  $this->one + $this->two + $this->three ;
-        echo  $this->one + $this->two + $this->three + $this->num2 ;
-        echo  $this->one + $this->two + $this->three + $this->num2 + $this->num3 ;        
-    }
-
-
-    $one->add(8,5,2,4);
-    $one->add(5,6,1);
+ $y = new x();
+ $y -> games();
 
 ?>
