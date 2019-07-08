2bf52f1f314fa6edcd9fc015213d22aa21a23712
nerf3388 <nerf3388@outlook.com>
2019-07-08 11:18:07 +0800

    j

--- WSLSK/Test.php
+++ WSLSK/Test.php
@@ -1,64 +1,16 @@
 <?php
-
-class Dice
+$serverName = "LSKPC\SQLEXPRESS";
+$connOptions = array("Database"=>"3.1","CharacterSet" => "UTF-8");
+ $conn = sqlsrv_connect( $serverName, $connOptions );
+if( $conn === false )
 {
-    public function Draw1() 
-    {  
-        echo '1';      
-    }    
-          
-    public function Draw2() 
-    {  
-        echo '2';      
-    }          
-
-    public function Draw3() 
-    {  
-        echo '3';      
-    }          
-
-    public function Draw4() 
-    {  
-        echo '4';      
-    }       
-
-    public function Draw5() 
-    {  
-        echo '5';      
-    }          
-
-    public function Draw6() 
-    {  
-        echo '6';      
-    }          
-
-
-function rollthedice()
+    echo "Could not close.\n"; 
+    die( print_r( sqlsrv_errors(), true));
+}
+else 
 {
-      $x=rand(1,6);
-      if ($x==1){     
-      $this->Draw1();
-      }
-
-      if ($x==2){     
-      $this->Draw2();
-      }
-
-      if ($x==3){     
-      $this->Draw3();
-      }
-
-      if ($x==4){     
-      $this->Draw4();
-      }
-
-      if ($x==5){     
-      $this->Draw5();
-      }
-      
-      if ($x==6){     
-      $this->Draw6();
-      }
+    echo "Connection close successfully!";
 }
-}
+
+sqlsrv_close( $conn );
 ?>
