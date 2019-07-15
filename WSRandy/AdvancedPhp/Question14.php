0f4751fabd0134ae07e3ce43105743635ff232bf
R_Kjz <randykjz@gmail.com>
2019-07-15 14:21:57 +0800

    uh

--- WSRandy/AdvancedPhp/Question14.php
+++ WSRandy/AdvancedPhp/Question14.php
@@ -1,12 +1,22 @@
 <?Php
 
+class Song
+{
+    public $song_name = "No any good songs for now";
+    
+    public function __construct($song_name)
+    {
+        echo "I'm good!";    
+        $this--->song_name = $song_name;
+    }
+    
+    public function __destruct()
+    {
+        echo "I'm bad now D:";
+    }
+}
 
-
-
-
-
-
-
-
+$song = new Song("A song is supposed to be good");
+echo "Name of the Song: " . $song->song_name;
 
 ?>
