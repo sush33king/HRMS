7c245b6e3aea9e14f83755e9b64d704f0e4b1c50
baijian <baijian743@gmail.com>
2019-07-18 13:32:18 +0800

    tan

--- WSbaijian/advanced/ClassFiles/12.php
+++ WSbaijian/advanced/ClassFiles/12.php
@@ -1,17 +1,6 @@
 <?php
-class game {
-    public function work(){
-    echo "this is game ";
-    }
-}
 
-class one extends  game{
-    public function work(){
-    echo "this is work";
-    }
-}
 
-$x = new one;
-$x->work();
+
 
 ?>
---
+++ WSbaijian/advanced/ClassFiles/15.php
@@ -0,0 +1,149 @@
+<?php
+class diagram
+{   
+    private $conn;
+
+    public function  connection(){
+        //setting the value for "serverName\instanceName"
+        $serverName = "LAPTOP-8KDQAATO\SQLEXPRESS"; 
+
+        //additional information for making connection to database
+        $connectionInfo = array( "Database"=>"purchase order", "UID"=>"sa", "PWD"=>"1234");
+
+        //using php function to connect to database using settings above
+        $this->conn = sqlsrv_connect( $serverName, $connectionInfo);
+
+        //check if connection is successful or not
+        if( $this->conn ) {
+            echo "Connection established.<br />";
+        }
+        
+        else{
+            echo "Connection could not be established.<br />";
+            die( print_r( sqlsrv_errors(), true));
+        }
+    }
+
+    public function querydb($str) {
+        $stmt = sqlsrv_query($this->conn, $str);  
+        if( $stmt === false )  
+        {  
+        echo "Error in query preparation/execution.\n";  
+        die( print_r( sqlsrv_errors(), true));  //this line of code terminates or ends the program completely
+        }     
+        $i = 0;
+        while( $obj = sqlsrv_fetch_object( $stmt))  
+        {  
+        $rs[$i++] = $obj;
+        /*echo $obj->LastName.", ".$obj->FirstName."<br>"; */      
+        }
+        return $rs ;
+    }
+
+    public function adddb($data, $tblName) {
+
+         //initialize variables to be used to store field names and field data
+        $fldNames = "";
+        $fldData =  "";
+    
+        //loop through array and extract field names and field data from array
+        foreach($data as $key => $val)
+        {
+                
+            if($fldNames == "")
+                {
+                    $fldNames = $key; 
+                    $fldData = "'" . $val . "'";  
+                }   
+                else
+                {                
+                    $fldNames = $fldNames .  ',' . $key;   
+                    $fldData = $fldData . ',' . "'" . $val . "'";     
+                }                                 
+            }
+    
+        //combine field names and field data values into sql query
+        $sql = "INSERT INTO " . $tblName . "(" . $fldNames . ") " . "VALUES(" . $fldData . ")";
+    
+        //execute prepared sql query
+        $stmt = sqlsrv_query( $this->conn, $sql);
+    
+        //if error then display error
+        if( $stmt === false )  
+        {  
+                echo "Error in query preparation/execution.\n";  
+                die( print_r( sqlsrv_errors(), true));  //this line of code terminates or ends the program completely
+        }  
+            else //if successful the display msg below
+        {
+                echo "Record successfully added!";
+        }
+    }    
+
+    public function deletedb($tblName, $where_condition) {
+
+        $sql = "DELETE FROM ".$tblName." WHERE ".$where_condition.""; 
+
+        $stmt = sqlsrv_query( $this->conn, $sql);
+    
+        if ($stmt === false) 
+        {
+        echo "Error deleting record:";
+        }       
+        else 
+        {
+        echo "Record deleted successfully" ;
+        }
+
+    }
+
+    public function update($tblName, $flddata, $where_condition) {
+        
+        $sql = "UPDATE ".$tblName." SET ".$flddata." WHERE ".$where_condition."";  
+
+        $stmt = sqlsrv_query( $this->conn, $sql);
+    
+        if ($stmt === false) 
+        {
+        echo "Error updating record:";
+        }       
+        else 
+        {
+        echo "Record update successfully" ;
+        }
+    }
+    public function closeconnection()
+    {
+        sqlsrv_close($this->conn) ;
+        if($this->conn === false)  
+        {
+            echo "Could Not disconnect."; 
+            die( print_r( sqlsrv_errors(), true));
+        }
+        else
+        {
+            echo "Successfully disconnected."; 
+        }
+        
+    }
+    
+
+}
+
+$conn = new diagram ;
+$conn -> connection();
+//$rs = $conn -> querydb('select * from tbl_purchase_order');
+//$data = array("Po_ID"=>"po0005", "Description"=>"good" ,"Po_Date"=>"2019-07-15 00:00:00" ,"customerID"=> "A0004","VendorID"=>"V0004" ) ;
+//$tblName = "tbl_purchase_order";
+//$conn->adddb($data, $tblName);
+//echo var_dump($rs);
+//$where_condition = "Po_ID = 'po0005'" ;
+//$tblName = "tbl_purchase_order";
+//$conn->deletedb($tblName, $where_condition);
+//$where_condition = "Po_ID = 'po0004' ";
+//$flddata = "Description = 'good' ";
+//$tblName = "tbl_purchase_order";
+//$conn->update($tblName, $flddata, $where_condition) ;
+$conn->closeconnection();
+
+?>
