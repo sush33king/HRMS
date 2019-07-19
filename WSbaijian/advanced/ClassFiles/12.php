8deb5411f293594edea6b685344ad6bc8071154c
baijian <baijian743@gmail.com>
2019-07-19 10:28:26 +0800

    tan

--- WSbaijian/advanced/ClassFiles/12.php
+++ WSbaijian/advanced/ClassFiles/12.php
@@ -1,6 +1,17 @@
 <?php
-
-
-
+class game
+{
+    public function _play($name,$date)
+    {
+        echo "fong play the game <br/>";
+    }
+   public static function _wacth($name,$date)
+   {
+       echo "fong wacth the video <br/>";
+   }
+}
+$addgame = new game;
+$addgame->gane();
+game::$date();
 
 ?>
