39409a24daa8b6726bdf5054e72fe73e0a45c460
R_Kjz <randykjz@gmail.com>
2019-07-19 12:17:29 +0800

    uhj

--- WSRandy/AdvancedPhp/Question14.php
+++ WSRandy/AdvancedPhp/Question14.php
@@ -7,7 +7,8 @@ class Song
     {
         echo "He's a good boi!<br>";
     }
-    
+
+    //destructor functions the same as construct but instantiates last
     public function __destruct()
     {
         echo "I'm the bad guy now D:<br>";
