479c6a988d8ee11d2810425f531810db15cea69b
nerf3388 <nerf3388@outlook.com>
2019-07-19 14:16:33 +0800

    d

---
+++ WSLSK/AdvancePhP/Question12.php
@@ -0,0 +1,38 @@
+<?php 
+  
+// Create Class
+class Cal  
+{ 
+      
+    // __call is a function which accepts function name and variable
+    function __call($name_of_function, $Variable1) { 
+              
+        // If function name is multiply
+        if($name_of_function == 'multiply') { 
+              
+            switch (count($Variable1)) { 
+                      
+                // If only two 
+                case 2: 
+                    return $Variable1[0] * $Variable1[1]; 
+                          
+                // If three 
+                case 3: 
+                   $temp = $Variable1[0] * $Variable1[1]; 
+                   return $temp * $Variable1[2];
+            } 
+        } 
+    } 
+} 
+      
+$cal = new Cal; 
+      
+// Multiply 
+echo "Multiply: ";
+echo($cal->multiply(5, 5)); //Show 25
+echo "<br><br>"; 
+      
+// Multiply 
+echo "Multiply: "; 
+echo ($cal->multiply(2, 2, 2)); //Show 8
+?> 
--- WSLSK/Test.php
+++ WSLSK/Test.php
@@ -1,28 +1,20 @@
 <?php
-class Msi {
-   function __construct() {
-       print "In Msi constructor\n";
-   }
+abstract class AbstractClass
+{
+    // Force Extending class to define this method
+    abstract protected function Class1();
+    public function Students() 
+    {
+        print $this->Class1() . "\n";
+    }
 }
 
-class laptop extends Msi {
-   function __construct() {
-       parent::__construct();
-       print "In laptop constructor\n";
-   }
+class Classes extends AbstractClass
+{
+    protected function Class1() 
+    {
+        return "Classes";
+    }  
 }
 
-class Illegear extends Msi {
-    // inherits games's constructor
-}
-
-// In games constructor
-$use = new Msi();
-
-// In games constructor
-// In laptop constructor
-$use = new laptop();
-
-// In games constructor
-//$use = illegear();
 ?>
