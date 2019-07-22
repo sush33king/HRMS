4dfcd3f14684351c5259f3fd42800cf0f5b40c20
chung97 <chung971004@gmail.com>
2019-07-23 02:30:04 +0800

    html

--- WSChung/web development/form.php
+++ WSChung/web development/form.php
@@ -2,6 +2,8 @@
 <html>
 <link   rel="stylesheet"
         href="https://fonts.googleapis.com/css?family=Butterfly+Kids&display=swap">
+<link   rel="stylesheet"
+        href="https://fonts.googleapis.com/css?family=Shadows+Into+Light&display=swap">
 <head>
 <style>
 p {
@@ -9,6 +11,19 @@ p {
   font-family: 'Butterfly Kids', cursive;
   font-size: 30px;
 }
+
+input[type=text] {
+  color: grey;
+  font-family: 'Shadows Into Light', cursive;
+  font-size: 20px
+}
+
+input[type=submit] {
+  color: black;
+  font-family: 'Butterfly Kids', cursive;
+  font-size: 20px
+}
+
 </style>
 </head>
 <body>
@@ -18,7 +33,9 @@ p {
   Address:<br>
   <input type="text" name="address" value="SHAH ALAM"><br>
   Age:<br>
-  <input type="text" name="age" value="100"><br><br>
+  <input type="text" name="age" value="18"><br>
+  School:<br>
+  <input type="text" name="school" value="ENLITEN"><br><br>
   <input type="submit" value="Submit">
 </form>    
 
