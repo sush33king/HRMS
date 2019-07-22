e7fa718e901383847abc017d67316604525a4ebf
chung97 <chung971004@gmail.com>
2019-07-23 02:19:43 +0800

    html

---
+++ WSChung/web development/form.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<link   rel="stylesheet"
+        href="https://fonts.googleapis.com/css?family=Butterfly+Kids&display=swap">
+<head>
+<style>
+p {
+  color: blue;
+  font-family: 'Butterfly Kids', cursive;
+  font-size: 30px;
+}
+</style>
+</head>
+<body>
+<form action="/action_page.php">
+  First name:<br>
+  <input type="text" name="name" value="LSK"><br>
+  Address:<br>
+  <input type="text" name="address" value="SHAH ALAM"><br>
+  Age:<br>
+  <input type="text" name="age" value="100"><br><br>
+  <input type="submit" value="Submit">
+</form>    
+
+<p>If you click the "Submit" button, the form-data will be sent to a page called "/action_page.php".</p>
+</body>
+</html>                                                                                                          
