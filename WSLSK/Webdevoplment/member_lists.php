731c357d16b84dd44f8640859a54999b1c2a8f6f
nerf3388 <nerf3388@outlook.com>
2019-08-14 13:47:44 +0800

    d

--- WSLSK/Webdevoplment/member_lists.php
+++ WSLSK/Webdevoplment/member_lists.php
@@ -3,20 +3,34 @@
 <head>
 <link rel="stylesheet" href="memberlists.css?version=2">
 </head>
-
-
-
-
-
-
-
-
-
 <body>
-        <div>
-        <form>
+<table border = 2>
+            <tr>
+                <th>First name</th>
+                <th>Last name</th>
+                <th>Age</th>
+                <th>Email</th>
+                <th>Address</th>
+                <th>Country</th>
+                <th>Height</th>
+                <th>Weight</th>
+                <th>Gender</th>
+                <th>Username</th>
+                <th>Password</th>
+            </tr>
+            <?php
+            include ('process.php');
 
-      </div>
+            //make connection
+            $conn = new process;
+            $conn -> connection();
 
+            //query
+            $query = ("select * from tbl_Member" );
+
+            $userdata = $conn->querydb($query);
+            
+            ?>
+        </table>
 </body>
 </html>
