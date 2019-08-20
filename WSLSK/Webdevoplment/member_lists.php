a9f499d9a8773936a76f4ea7b0f907ae72e4572d
nerf3388 <nerf3388@outlook.com>
2019-08-20 12:19:27 +0800

    l

--- WSLSK/Webdevoplment/member_lists.php
+++ WSLSK/Webdevoplment/member_lists.php
@@ -4,7 +4,7 @@
 <link rel="stylesheet" href="memberlists.css?version=1">
 </head>
 <body>
-            <?php
+        <?php
             
             include ('process.php');
 
@@ -17,22 +17,25 @@
 
             $userdata = $conn->querydb($query);
             
-            ?>
+        ?>
+
+        <input type="text" name="search">
+        <input type="submit" name="submit" value="Search">
             
-            <?php
-  echo "<body>";
-  echo "<table align = center ; id=myTable>";
-  echo "  <tr>
-            <th>Name</th>
-            <th>Age</th>
-            <th>Email</th>
-            <th>Address</th>
-            <th>Country</th>
-            <th>Height</th>
-            <th>Weight</th>
-            <th>Gender</th>
-        </tr> " ;
-         echo "<tr>";
+        <?php
+                echo "<body>";
+                echo "<table align = center ; id=myTable>";
+                echo "  <tr>
+                <th>Name</th>
+                <th>Age</th>
+                <th>Email</th>
+                <th>Address</th>
+                <th>Country</th>
+                <th>Height</th>
+                <th>Weight</th>
+                <th>Gender</th>
+                </tr> " ;
+                echo "<tr>";
          
 foreach($userdata as $item){
    echo "<tr>";
