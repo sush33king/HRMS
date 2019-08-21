faa5b1be1be5516929346d94447aa3a2cc9dd52c
chung97 <chung971004@gmail.com>
2019-08-21 15:22:21 +0800

    php

--- WSChung/web development/login_process.php
+++ WSChung/web development/login_process.php
@@ -1,6 +1,7 @@
 <?php
 
 include ('data.php');
+include ('Sanitize.php');
 
 if(isset($_POST['Submit']))
 {
@@ -19,6 +20,11 @@ if(isset($_POST['Submit']))
 
         else
         {
+            //sanitization the input
+            $sanitize = new sanitization;
+            $user = $sanitize->clean($user);
+            $pass = $sanitize->clean($pass);
+
             //Query
             $query = ("select * FROM tbl_menber WHERE fld_Username = '". $user ."' 
             AND fld_Password = '" . $pass . "'");
